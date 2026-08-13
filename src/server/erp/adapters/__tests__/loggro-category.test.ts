f32d1333188a622bd15b912f1d0ab57433f366ec
Jorge Agudelo <ja0435795@gmail.com>
2026-08-13 12:01:47 -0500

    test: cover sellable Loggro accessories

--- src/server/erp/adapters/__tests__/loggro-category.test.ts
+++ src/server/erp/adapters/__tests__/loggro-category.test.ts
@@ -15,4 +15,28 @@ describe("detectLoggroCategory", () => {
       name: "Chanclas y Sandalias",
     })
   })
+
+  it("clasifica accesorios vendibles sin confundir calzado, empaque u obsequios", () => {
+    const accessories = [
+      "GORRA NEW ERA NEYYAN ROJO",
+      "MOCHILA VANS BENCHED BAG BLANCO NEGRO",
+      "MALETIN DISCOVERY AZUL LONA CORDONES",
+      "VANS CINTURON DE TELA NEGRO",
+      "VANS CORDONES NEGRO",
+      "CLASSIC CREW SOCKS MEDIAS LARGAS TODA NEGRAS",
+      "RESHOEVN8R ORIGINAL CLEANING KIT",
+    ]
+
+    for (const name of accessories) {
+      expect(detectLoggroCategory(name)).toEqual({
+        slug: "accesorios",
+        name: "Accesorios",
+      })
+    }
+
+    expect(detectLoggroCategory("MEDIA BOTA CONVERSE HIGH STREET")).toBeUndefined()
+    expect(detectLoggroCategory("BOLSA TELA GRIS GRANDE")).toBeUndefined()
+    expect(detectLoggroCategory("MEDIAS CONVERSE OBSEQUIOS")).toBeUndefined()
+    expect(detectLoggroCategory("TENIS INFANTIL ROSADO BOLSO")).toBeUndefined()
+  })
 })
